6ec33f2a7821d0d49ede832803e983bfe0355f9b
Tarun Agarwal <tarun.agarwal@gocomet.com>
2021-05-09 22:31:20 +0530

    corrected url

--- api/candidate/urls.py
+++ api/candidate/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
   path('signup', Signup.as_view()),
   path('login', Login.as_view()),
-  path('all_applied', AllApplied.as_view()),
+  path('all_applied/<str:id>', AllApplied.as_view()),
   path('applied/<str:id>', AppliedDetails.as_view()),
   path('book_slot', BookSlot.as_view()),
 ]
